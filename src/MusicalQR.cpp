26b29712e56e33a256ccbace79f4d03264070512
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-09-29 00:18:56 -0400

    initial commit of files

---
+++ src/MusicalQR.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MusicalQR.cpp
+// Author      : Jacob Reiss (HoneyJar_)
+// Version     :
+// Copyright   : Copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Cilantro" << endl; // prints Cilantro
+	return 0;
+}
